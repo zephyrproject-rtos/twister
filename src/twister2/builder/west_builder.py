a8c6b8be596daed59bedbb90f301b550ebc3895d
Fundakowski, Lukasz <lukasz.fundakowski@nordicsemi.no>
2022-12-09 09:55:19 +0100

    Improve logging for WestBuilder

--- src/twister2/builder/west_builder.py
+++ src/twister2/builder/west_builder.py
@@ -17,8 +17,7 @@ class WestBuilder(BuilderAbstract):
         """
         Build Zephyr application with `west`.
         """
-        west = shutil.which('west')
-        if west is None:
+        if (west := shutil.which('west')) is None:
             raise TwisterBuildException('west not found')
 
         command = [
@@ -41,19 +40,28 @@ class WestBuilder(BuilderAbstract):
             process = subprocess.run(
                 command,
                 stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
             )
         except subprocess.CalledProcessError as e:
-            logger.error('Failed building %s for %s', build_config.source_dir, build_config.platform)
+            logger.exception(
+                'An exception has been raised for build subprocess: %s for %s',
+                build_config.source_dir, build_config.platform
+            )
             raise TwisterBuildException('Building error') from e
         else:
             if process.returncode == 0:
+                self._log_output(process.stdout, logging.DEBUG)
                 logger.info('Finished building %s for %s', build_config.source_dir, build_config.platform)
             else:
-                logger.error(process.stderr.decode())
-                raise TwisterBuildException(
-                    f'Failed building {build_config.source_dir} for platform: {build_config.platform}'
-                )
+                self._log_output(process.stdout, logging.INFO)
+                msg = f'Failed building {build_config.source_dir} for platform: {build_config.platform}'
+                logger.error(msg)
+                raise TwisterBuildException(msg)
+
+    @staticmethod
+    def _log_output(output: bytes, level: int) -> None:
+        for line in output.decode().split('\n'):
+            logger.log(level, line)
 
     @staticmethod
     def _prepare_cmake_args(cmake_args: list[str]) -> str:
